22b4256f713f2eb032f64296c40aef1cc773cae5
S14mx <sergey.otryshko@gmail.com>
2022-08-17 17:25:21 -0700

    completed lab 28

--- snacks/models.py
+++ snacks/models.py
@@ -7,6 +7,8 @@ class Snack(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=128, default="")
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
